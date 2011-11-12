30f7854671607360ba9d4332ced96b2b46d2c3a3
t.michelbach <t.michelbach@gmail.com>
2011-11-12 22:02:41 +0100

    Mock Badge service

---
+++ grails-app/services/spine/BadgeService.groovy
@@ -0,0 +1,16 @@
+package spine
+
+class BadgeService {
+
+    static transactional = false
+
+    /**
+     * based upon tag list, badges will be returned; at the moment we just hardcore some value
+     * the badges are basically strings, which need a corresponding picture with an added .jpg
+     * 
+     * @return
+     */
+	def evaluateTags(HashMap taglist) {
+
+    }
+}
