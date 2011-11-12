45e9bb9cebea5b549951bfb0b3c689879280cfe4
t.michelbach <t.michelbach@gmail.com>
2011-11-12 22:00:14 +0100

    User domain fixed camel notation for properties

--- grails-app/domain/spine/User.groovy
+++ grails-app/domain/spine/User.groovy
@@ -4,13 +4,22 @@ class User {
 
 	Long id
 	Long version
+	String lastName
+	String firstName
 	String email
+	String company
 	String password
+	String country
+	String city
+	String imagePath
+	String freeText
 	String toString()
 	{ "$email" }
 	def constraints =
 	{
 		email(email:true)
+		lastName(blank:false)
+		firstName(blank:false)
 		password(blank:false, password:true)
 	}
 }
