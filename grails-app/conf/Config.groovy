fa34d4c342b5b5eabf4690440642d8b61370a5c6
Stas <stanislav.shevchenko@me.com>
2011-11-17 18:51:04 +0200

    1. Refactored GraphCommunicatorService 2. Added logging 3. Added dependency Injection usage 4. Refactored Configuration

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -13,19 +13,19 @@
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
 grails.mime.use.accept.header = false
-grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
-                      xml: ['text/xml', 'application/xml'],
-                      text: 'text/plain',
-                      js: 'text/javascript',
-                      rss: 'application/rss+xml',
-                      atom: 'application/atom+xml',
-                      css: 'text/css',
-                      csv: 'text/csv',
-                      all: '*/*',
-                      json: ['application/json','text/json'],
-                      form: 'application/x-www-form-urlencoded',
-                      multipartForm: 'multipart/form-data'
-                    ]
+grails.mime.types = [html: ['text/html', 'application/xhtml+xml'],
+        xml: ['text/xml', 'application/xml'],
+        text: 'text/plain',
+        js: 'text/javascript',
+        rss: 'application/rss+xml',
+        atom: 'application/atom+xml',
+        css: 'text/css',
+        csv: 'text/csv',
+        all: '*/*',
+        json: ['application/json', 'text/json'],
+        form: 'application/x-www-form-urlencoded',
+        multipartForm: 'multipart/form-data'
+]
 
 // URL Mapping Cache Max Size, defaults to 5000
 //grails.urlmapping.cache.maxsize = 1000
@@ -65,26 +65,32 @@ environments {
 
 }
 
-// log4j configuration
+neo4j.url = 'http://localhost:7474'
+
 log4j = {
-    // Example of changing the log pattern for the default console
-    // appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
+    environments {
+        development {
+            appenders {
+                console name: 'stdout', layout: pattern(conversionPattern: '%c{2} %m%n')
+            }
 
-    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
-           'org.codehaus.groovy.grails.web.pages', //  GSP
-           'org.codehaus.groovy.grails.web.sitemesh', //  layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping', // URL mapping
-           'org.codehaus.groovy.grails.commons', // core / classloading
-           'org.codehaus.groovy.grails.plugins', // plugins
-           'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
+            all 'spine',
+                    'grails.app.controller',
+                    'grails.app.service'
+        }
+    }
 
-    warn   'org.mortbay.log'
+    error 'org.codehaus.groovy.grails.web.servlet',  //  controllers
+            'org.codehaus.groovy.grails.web.pages', //  GSP
+            'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+            'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+            'org.codehaus.groovy.grails.web.mapping', // URL mapping
+            'org.codehaus.groovy.grails.commons', // core / classloading
+            'org.codehaus.groovy.grails.plugins', // plugins
+            'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+            'org.springframework',
+            'org.hibernate',
+            'net.sf.ehcache.hibernate'
+
+    warn 'org.mortbay.log'
 }
--- grails-app/services/spine/SpineService.groovy
+++ grails-app/services/spine/SpineService.groovy
@@ -3,291 +3,287 @@ package spine
 class SpineService {
 
     static transactional = false
-	def networkService = new NetworkService()
-	def badgeService = new BadgeService()
-	
-	/**
-	 * take the email address and the password and verify, if exist in database and correct
-	 * if yes then instantiate the loggedInUser object, if not, return null
-	 * 
-	 * @param email
-	 * @param password
-	 * @return user as User object
-	 */
-	def loginUser(String email, String password) {
+    NetworkService networkService
+    BadgeService badgeService
 
-		def user
-		
-		// retrieve the node via email address
-		def userNode = networkService.readNode(email)
+    /**
+     * take the email address and the password and verify, if exist in database and correct
+     * if yes then instantiate the loggedInUser object, if not, return null
+     *
+     * @param email
+     * @param password
+     * @return user as User object
+     */
+    def loginUser(String email, String password) {
 
-		// verify if user exists and if passwords are identical
-		if (userNode != null)
-			if (userNode.password == password) {
-				
-				//create an instance of the user
-				user = new User()
-				
-				// copy over the values from the hash map into the user object
-				user.firstName = userNode.firstName
-				user.lastName = userNode.lastName
-				user.email = userNode.email
-				user.country = userNode.country
-				user.city = userNode.city
-				user.imagePath = userNode.image
-				user.freeText = 'My biography'
-				user.tags = networkService.getIncomingTagsForNode(userNode.email)
-				if (user.tags != null)
-					user.badges = badgeService.evaluateTags(user.tags)
-			}
-	
-		//returns either the loggedInUser or null, if login was not successful
-		return user
-	}
+        def user
 
-	
-	/**
-	 * 
-	 * @param contextUser
-	 * @param filter
-	 * @param offset
-	 * @return userList of type User
-	 */
-	def getUserNetwork(User contextUser, String filter, int offset) {
+        // retrieve the node via email address
+        def userNode = networkService.readNode(email)
 
-		def queryReturn
-		def userList = []
+        // verify if user exists and if passwords are identical
+        if (userNode != null)
+            if (userNode.password == password) {
 
-		// verify if a filter has been passed
-		
-		if ((filter == null) || (filter == '')) {
-			
-			// get the neighbours in batches of 20
-			queryReturn = networkService.queryForNeighbourNodes(contextUser.email, offset, 20)
+                //create an instance of the user
+                user = new User()
 
-		}
-		else {
-		
-			// first step is to tokenize the filter string
-			def tokens = " ,;"
-			def wordList = []
-			wordList = filter.tokenize(tokens)
-			
-			println "search filter: "+wordList
-			
-			// now we need to wait until the network service queryNode is ready, as this is not the case yet, use the same service as in the if
-			queryReturn = networkService.queryForNeighbourNodes(contextUser.email, offset, 20)
-		}
-		
-		// loop through the list and instantiate the user objects incl. tags
-		def user
-		
-		queryReturn.each {
-			user = new User()
-			user.firstName = it.firstName
-			user.lastName = it.lastName
-			user.email = it.email
-			user.country = it.country
-			user.city = it.city
-			user.imagePath = it.image
-			user.freeText = 'My biography'
-			user.tags = networkService.getIncomingTagsForNode(it.email)
-			user.distance = it.distance
-			if (user.tags != null)
-				user.badges = badgeService.evaluateTags(user.tags)
+                // copy over the values from the hash map into the user object
+                user.firstName = userNode.firstName
+                user.lastName = userNode.lastName
+                user.email = userNode.email
+                user.country = userNode.country
+                user.city = userNode.city
+                user.imagePath = userNode.image
+                user.freeText = 'My biography'
+                user.tags = networkService.getIncomingTagsForNode(userNode.email)
+                if (user.tags != null)
+                    user.badges = badgeService.evaluateTags(user.tags)
+            }
 
-			userList.add(user)
-		}
-
-		return userList
+        //returns either the loggedInUser or null, if login was not successful
+        return user
     }
 
-	/**
-	 * returns a User object based on email address
-	 * 
-	 * @return
-	 */
-	def getUser(String email) {
-		
-		// instantiate return structure
-		def user = new User()
+    /**
+     *
+     * @param contextUser
+     * @param filter
+     * @param offset
+     * @return userList of type User
+     */
+    def getUserNetwork(User contextUser, String filter, int offset) {
 
-		// retrieve the properties
-		def userNode = networkService.readNode(email)
-		
-		// copy over the values from the hash map into the user object
-		user.firstName = userNode.firstName
-		user.lastName = userNode.lastName
-		user.email = userNode.email
-		user.country = userNode.country
-		user.city = userNode.city
-		user.imagePath = userNode.image
-		user.freeText = 'My biography'
-		user.tags = networkService.getIncomingTagsForNode(userNode.email)
-		if (user.tags != null)
-			user.badges = badgeService.evaluateTags(user.tags)
+        def queryReturn
+        def userList = []
 
-		// add something related to distance to the current contextUser
-		
-		return user
-	}
+        // verify if a filter has been passed
 
-	/**
-	 * retrieve the list of tags of all incoming connections to a given user, incl. amount and sorted (highest amount first)
-	 * 
-	 * @param user
-	 * @param amount
-	 * @return userTagMap
-	 */
-	def getUserTags(User user) {
+        if ((filter == null) || (filter == '')) {
 
-		def userTagMap = [:]
+            // get the neighbours in batches of 20
+            queryReturn = networkService.queryForNeighbourNodes(contextUser.email, offset, 20)
 
-		// get the tags
-		userTagMap = networkService.getIncomingTagsForNode(user.email)
-		
-		return userTagMap
-	}
-	
-	/**
-	 * create a new user in the network, which from an UI perspective only works with adding a relationship from myself and tagging it
-	 * 
-	 * @param userparams
-	 * @param tags
-	 * @return success or fail
-	 */
-	def createNewUser(HashMap userparams, List tags) {
-		
-		def newUser = new User()
-		def success = false
+        }
+        else {
 
-		// copy over the values from the hash map into the user object to trigger validation
-		newUser.firstName = userparams.firstName
-		newUser.lastName = userparams.lastName
-		newUser.password = userparams.password
-		newUser.email = userparams.email
-		newUser.city = userparams.city
-		newUser.country = userparams.country
-		newUser.imagePath = userparams.image
-		//newUser.freeText = "Free Text"
-		
-		// set over into map for call
-		def userProps = ['firstName' : newUser.firstName,
-							'lastName' : newUser.lastName, 
-							'city' : newUser.city,
-							'country' : newUser.country,
-							'email' : newUser.email,
-							'password' : newUser.password,
-							'image' : newUser.imagePath]
-				
-		// verify if node with same email does not exist already
-		if (networkService.readNode(newUser.email) != null)
-			success = false
-		else {
-					
-			// create the node
-			def userNode = networkService.createNode(userProps)
-			
-			// create the node
-			if (userNode != null) {
-				
-				// if a tag list has been provided, this means that a relationship should be created with these tags
-				if (tags != null) {
-					def currentUser = session.user
-					success = setTag(currentUser, newUser, tags)
-				}
-				
-				success = true
-			}
-		}
-		
-		return success
-	}
-	
+            // first step is to tokenize the filter string
+            def tokens = " ,;"
+            def wordList = []
+            wordList = filter.tokenize(tokens)
 
-	/**
-	 * Update node properties
-	 * 
-	 * @param loggedInUser
-	 * @param properties
-	 * @return success
-	 */
-	def updateUserProfile(User loggedInUser, HashMap properties) {
-	
-		def success = new Boolean()
-			
-		// update node properties
-		
-		return success
-	}
-	
-	
-	/**
-	 * Add a tag to an existing relationship or create a new one
-	 * 
-	 * @param loggedInUser = User
-	 * @param targetUser = Email
-	 * @param taglist = space , ; separated list
-	 * @return success
-	 */
-	def addTag(User loggedInUser, String targetUser, String tags) {
-		
-		// tokenize taglist, then check, if relationship exists, if yes then update, if not then create new one
-		
-		def success = new Boolean()
-		def parameters = new HashMap()
-		
-		parameters.put('startNode',loggedInUser.email)
-		parameters.put('endNode',targetUser)
-		parameters.put('tags',tags)
-		
-		//Todo: Relationship exists?
-		if (networkService.readRelationship() == null){
-			networkService.createRelationship(parameters)
-		}
-		networkService.setProperty(parameters)
-		
-		return success=true
-	}
-	
-	/**
-	 * Remove a tag from relationship and delete, if non left
-	 * 
-	 * @param loggedInUser
-	 * @param targetUser
-	 * @param taglist
-	 * @return sucess
-	 */
-	def removeTag(User loggedInUser, User targetUser, String tag) {
-		
-		// remove tag from relationship properties; if no property left, delete relationship
-		
-		def success = new Boolean()
-		def parameters = new HashMap()
-		
-		parameters.put('startNode',loggedInUser)
-		parameters.put('endNode',targetUser)
-		parameters.put('tag',tag)
-		
-		success = networkService.deleteProperty(parameters)
-		
-		
-		return success
-	}
-	
-	
-	/**
-	 * Calls the method evaluateBadgeRules from the badge service based on a list of tags provided
-	 * 
-	 * @param user
-	 * @return badgeList
-	 */
-	def getBadges(TreeMap tags) {
-		
-		def badgeList = ['javahero', 'godofhtml']
-		
-		// retrieve list of badges, actually just a list of imagepaths
-		
-		return badgeList
-	}
+            println "search filter: " + wordList
+
+            // now we need to wait until the network service queryNode is ready, as this is not the case yet, use the same service as in the if
+            queryReturn = networkService.queryForNeighbourNodes(contextUser.email, offset, 20)
+        }
+
+        // loop through the list and instantiate the user objects incl. tags
+        def user
+
+        queryReturn.each {
+            user = new User()
+            user.firstName = it.firstName
+            user.lastName = it.lastName
+            user.email = it.email
+            user.country = it.country
+            user.city = it.city
+            user.imagePath = it.image
+            user.freeText = 'My biography'
+            user.tags = networkService.getIncomingTagsForNode(it.email)
+            user.distance = it.distance
+            if (user.tags != null)
+                user.badges = badgeService.evaluateTags(user.tags)
+
+            userList.add(user)
+        }
+
+        return userList
+    }
+
+    /**
+     * returns a User object based on email address
+     *
+     * @return
+     */
+    def getUser(String email) {
+
+        // instantiate return structure
+        def user = new User()
+
+        // retrieve the properties
+        def userNode = networkService.readNode(email)
+
+        // copy over the values from the hash map into the user object
+        user.firstName = userNode.firstName
+        user.lastName = userNode.lastName
+        user.email = userNode.email
+        user.country = userNode.country
+        user.city = userNode.city
+        user.imagePath = userNode.image
+        user.freeText = 'My biography'
+        user.tags = networkService.getIncomingTagsForNode(userNode.email)
+        if (user.tags != null)
+            user.badges = badgeService.evaluateTags(user.tags)
+
+        // add something related to distance to the current contextUser
+
+        return user
+    }
+
+    /**
+     * retrieve the list of tags of all incoming connections to a given user, incl. amount and sorted (highest amount first)
+     *
+     * @param user
+     * @param amount
+     * @return userTagMap
+     */
+    def getUserTags(User user) {
+
+        def userTagMap = [:]
+
+        // get the tags
+        userTagMap = networkService.getIncomingTagsForNode(user.email)
+
+        return userTagMap
+    }
+
+    /**
+     * create a new user in the network, which from an UI perspective only works with adding a relationship from myself and tagging it
+     *
+     * @param userparams
+     * @param tags
+     * @return success or fail
+     */
+    def createNewUser(HashMap userparams, List tags) {
+
+        def newUser = new User()
+        def success = false
+
+        // copy over the values from the hash map into the user object to trigger validation
+        newUser.firstName = userparams.firstName
+        newUser.lastName = userparams.lastName
+        newUser.password = userparams.password
+        newUser.email = userparams.email
+        newUser.city = userparams.city
+        newUser.country = userparams.country
+        newUser.imagePath = userparams.image
+        //newUser.freeText = "Free Text"
+
+        // set over into map for call
+        def userProps = ['firstName': newUser.firstName,
+                'lastName': newUser.lastName,
+                'city': newUser.city,
+                'country': newUser.country,
+                'email': newUser.email,
+                'password': newUser.password,
+                'image': newUser.imagePath]
+
+        // verify if node with same email does not exist already
+        if (networkService.readNode(newUser.email) != null)
+            success = false
+        else {
+
+            // create the node
+            def userNode = networkService.createNode(userProps)
+
+            // create the node
+            if (userNode != null) {
+
+                // if a tag list has been provided, this means that a relationship should be created with these tags
+                if (tags != null) {
+                    def currentUser = session.user
+                    success = setTag(currentUser, newUser, tags)
+                }
+
+                success = true
+            }
+        }
+
+        return success
+    }
+
+    /**
+     * Update node properties
+     *
+     * @param loggedInUser
+     * @param properties
+     * @return success
+     */
+    def updateUserProfile(User loggedInUser, HashMap properties) {
+
+        def success = new Boolean()
+
+        // update node properties
+
+        return success
+    }
+
+    /**
+     * Add a tag to an existing relationship or create a new one
+     *
+     * @param loggedInUser = User
+     * @param targetUser = Email
+     * @param taglist = space , ; separated list
+     * @return success
+     */
+    def addTag(User loggedInUser, String targetUser, String tags) {
+
+        // tokenize taglist, then check, if relationship exists, if yes then update, if not then create new one
+
+        def success = new Boolean()
+        def parameters = new HashMap()
+
+        parameters.put('startNode', loggedInUser.email)
+        parameters.put('endNode', targetUser)
+        parameters.put('tags', tags)
+
+        //Todo: Relationship exists?
+        if (networkService.readRelationship() == null) {
+            networkService.createRelationship(parameters)
+        }
+        networkService.setProperty(parameters)
+
+        return success = true
+    }
+
+    /**
+     * Remove a tag from relationship and delete, if non left
+     *
+     * @param loggedInUser
+     * @param targetUser
+     * @param taglist
+     * @return sucess
+     */
+    def removeTag(User loggedInUser, User targetUser, String tag) {
+
+        // remove tag from relationship properties; if no property left, delete relationship
+
+        def success = new Boolean()
+        def parameters = new HashMap()
+
+        parameters.put('startNode', loggedInUser)
+        parameters.put('endNode', targetUser)
+        parameters.put('tag', tag)
+
+        success = networkService.deleteProperty(parameters)
+
+
+        return success
+    }
+
+    /**
+     * Calls the method evaluateBadgeRules from the badge service based on a list of tags provided
+     *
+     * @param user
+     * @return badgeList
+     */
+    def getBadges(TreeMap tags) {
+
+        def badgeList = ['javahero', 'godofhtml']
+
+        // retrieve list of badges, actually just a list of imagepaths
+
+        return badgeList
+    }
 }
