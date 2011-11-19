34f1d2252d5afe79bad365dc7a421ce688c8216c
nicetime <jure@zakotnik.de>
2011-11-19 22:26:51 +0100

    Merged Stas' changes. Tests were moved to Integration testing, so just run test-app, this should result in 35 tests passed.

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,5 @@
-// Place your Spring DSL code here
+import static org.codehaus.groovy.grails.commons.ConfigurationHolder.getConfig
+
 beans = {
+    httpBuilder(groovyx.net.http.HTTPBuilder, config.neo4j.url)
 }
--- grails-app/services/spine/ImportDataService.groovy
+++ grails-app/services/spine/ImportDataService.groovy
@@ -3,37 +3,37 @@ package spine
 class ImportDataService {
 
     static transactional = false
-	def n = new NetworkService()
+    NetworkService networkService
 
     def importEdges(String file) {
-		println 'Starting to connect nodes...'
-		def props = [:]
-		def input = file.splitEachLine("\t") {fields ->
-			props['startNode'] = fields[0]
-			props['endNode'] = fields[1]
-			props['tags'] = fields[2]
-			println 'Connecting : ' + fields[0] + ' -> '+ fields[1] + ' with ' + fields[2]
-			n.createRelationship(props)
-			n.setProperty(props)
-		}
-		println "Edges loaded!"
-	}
-	
-	def importNodes(String file) {
-		//per node from file, create a hashmap with properties, which are imported
-		def input = file.splitEachLine("\t") {fields ->
-			//println fields
-			def props = [:]
-			props['firstName'] = fields[0]
-			props['lastName'] = fields[1]
-			props['email'] = fields[2]
-			props['password'] = fields[3]
-			props['city'] = fields[4]
-			props['country'] = fields[5]
-			props['image'] = fields[6]
-			props['freeText'] = 'Madness? This is Sparta!'
-			n.createNode(props)
-		}
-		println "Nodes created!"
-	}
+        println 'Starting to connect nodes...'
+        def props = [:]
+        def input = file.splitEachLine("\t") {fields ->
+            props['startNode'] = fields[0]
+            props['endNode'] = fields[1]
+            props['tags'] = fields[2]
+            println 'Connecting : ' + fields[0] + ' -> ' + fields[1] + ' with ' + fields[2]
+            networkService.createRelationship(props)
+            networkService.setProperty(props)
+        }
+        println "Edges loaded!"
+    }
+
+    def importNodes(String file) {
+        //per node from file, create a hashmap with properties, which are imported
+        def input = file.splitEachLine("\t") {fields ->
+            //println fields
+            def props = [:]
+            props['firstName'] = fields[0]
+            props['lastName'] = fields[1]
+            props['email'] = fields[2]
+            props['password'] = fields[3]
+            props['city'] = fields[4]
+            props['country'] = fields[5]
+            props['image'] = fields[6]
+            props['freeText'] = 'Madness? This is Sparta!'
+            networkService.createNode(props)
+        }
+        println "Nodes created!"
+    }
 }
