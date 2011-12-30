b36cf518c45e20509cb43d60547507ac7c00e6f2
t.michelbach <t.michelbach@gmail.com>
2011-12-30 13:16:38 +0100

    Minor change

--- grails-app/services/spine/SmtpService.groovy
+++ grails-app/services/spine/SmtpService.groovy
@@ -8,7 +8,7 @@ class SmtpService {
 	
 	grails.plugin.mail.MailService mailService 
 	
-	private sendRegistrationMail(String toMail, String fromMail){
+	private sendAccountActivationMail(String toMail, String fromMail){
 		
 		mailService.sendMail {
 			//multipart true
@@ -16,9 +16,9 @@ class SmtpService {
 			from fromMail
 			subject "Hello John"
 			body(
-				view: "/emails/account-registration",
+				view: "/emails/account-activation",
 				//plugin:"email-confirmation",
-				model:[fromAddress:'t.michelbach@gmail.com', id:'111']
+				model:[fromAddress:'t.michelbach@gmail.com', id: toMail]
 			)			
 		    //attachBytes 'spine-logo.gif','image/gif', new File(  "./web-app/images/emails/spine-logo.gif").readBytes()
 		  }
@@ -67,7 +67,7 @@ class SmtpService {
 			body(
 				view: "/emails/password-recovery",
 				//plugin:"email-confirmation",
-				model:[fromAddress:'t.michelbach@gmail.com', id:'111']
+				model:[fromAddress:'t.michelbach@gmail.com', id: toMail]
 			)
 			//attachBytes 'spine-logo.gif','image/gif', new File(  "./web-app/images/emails/spine-logo.gif").readBytes()
 		  }
