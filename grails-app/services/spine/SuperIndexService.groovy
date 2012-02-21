b2e9c5eaf08bafa7d81c3964b3c99f771e0dcfdc
t.michelbach <t.michelbach@gmail.com>
2012-02-21 21:33:50 +0100

    Super Index

---
+++ grails-app/services/spine/SuperIndexService.groovy
@@ -0,0 +1,95 @@
+package spine
+
+/**
+ * This is a first attempt of using a good index to make search work better
+ *
+ */
+
+class SuperIndexService {
+
+	static transactional = true
+	
+	def graphCommunicatorService
+
+	def addNodeToSuperIndex(String key, String value, String nodeURI) {
+		def indexPath = '/db/data/index/node/super_index/'
+		def requestQuery = [ 'key' : key,
+					'value' : value,
+					'uri' : nodeURI ]
+		graphCommunicatorService.neoPost(indexPath, requestQuery)
+	}
+
+	def addTagToIndex(String tag, String nodeURI) {
+		addNodeToSuperIndex("tag", tag, nodeURI)
+	}
+
+	def addBadgeToIndex(String badge, String nodeURI) {
+		addNodeToSuperIndex("badge", badge, nodeURI)
+	}
+
+	def addEmailToIndex(String email, String nodeURI) {
+		addNodeToSuperIndex("email", email, nodeURI)
+	}
+	
+	def addCityToIndex(String city, String nodeURI) {
+		addNodeToSuperIndex("city", city, nodeURI)
+	}
+	
+	def addFirstNameToIndex(String firstName, String nodeURI) {
+		addNodeToSuperIndex("firstname", firstName, nodeURI)
+	}
+	
+	def addLastNameToIndex(String lastName, String nodeURI) {
+		addNodeToSuperIndex("lastname", lastName, nodeURI)
+	}
+	
+	/**
+	 * Insert / update the data aboute the node in the super index
+	 * @param nodeID
+	 * @param json
+	 * @return
+	 */
+	def indexNode(String nodeURI, json = null) {
+		if(json == null) {
+			json = graphCommunicatorService.neoGet(nodeURI)
+		}
+		
+		// Indexing tags of the node
+		def tags = []
+		def incomingRelationshipsJson = graphCommunicatorService.neoGet(json.incoming_relationships);
+		incomingRelationshipsJson.each 
+		{
+			it.data.each { 
+				if(!tags.contains(it.key))
+				{
+					tags.add(it.key)
+				}
+			}
+		}
+		tags.each { addTagToIndex(it, nodeURI) }
+		
+		// Index user basic data
+		if(json.data?.email)  // adding email
+			addFirstNameToIndex(json.data.email, nodeURI)
+		
+		if(json.data?.firstName)  // adding first name
+			addFirstNameToIndex(json.data.firstName, nodeURI)
+		
+		if(json.data?.lastName)  // adding last name
+			addLastNameToIndex(json.data.lastName, nodeURI)
+		
+		if(json.data?.city)  // adding city
+			addCityToIndex(json.data.city, nodeURI)
+		
+		
+	}
+	
+	/**
+	 * Re-index everything ! 
+	 * @return
+	 */
+	def indexAll() {
+		def json = graphCommunicatorService.neoGet('/db/data/index/node/names', ['query': 'email:*'])
+		json.each { indexNode(it.self, it) }
+	}
+}
