ff87378678516482f6c867f493c04a3722b0285c
jure.zakotnik <jure.zakotnik@192.168.64.1>
2012-10-25 21:48:59 +0200

    Added default Tags file and handling

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,11 +1,14 @@
 import spine.SpineService;
+import spine.NetworkService;
 
 class BootStrap {
 
 	SpineService spineService
+	NetworkService networkService
 	
     def init = { 
 		spineService.getHotTags();
+		networkService.initDefaultTags();
     }
 	
     def destroy = {
